a16e2cde3b9eb83d7650d211d738d07170543f4b
Ivan Fomin <la-verne@mail.ru>
2017-05-19 19:59:29 +0300

    HomeTask5

---
+++ home5.sql
@@ -0,0 +1,31 @@
+SELECT categories.name, (
+SELECT AVG(products.price)
+FROM products
+WHERE categories.id = products.categor_id
+) AS Price
+FROM categories
+ORDER BY categories.name;
+
+SELECT categories.name, (
+SELECT AVG(products.price) 
+FROM products 
+WHERE categories.id = products.categor_id 
+AND products.counts > 0 )
+AS Price 
+FROM categories 
+ORDER BY categories.name
+
+CREATE TABLE `DBA`.`orders` ( `id` SERIAL NOT NULL , `timeAndDate` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,product_id BIGINT(10) UNSIGNED NOT NULL  , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+
+ALTER TABLE orders
+ADD FOREIGN KEY(product_id)
+REFERENCES products(id)
+ON DELETE RESTRICT
+ON UPDATE CASCADE;
+
+SELECT cast(orders.timeAndDate as date) AS DATE, 
+COUNT(*),SUM(products.price) AS SUM FROM `orders`,products 
+WHERE orders.product_id = products.id GROUP BY 1
+
+
+
