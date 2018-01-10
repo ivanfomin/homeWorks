c5a38d450a80eaa6125b0b07a31fd3fd35e2d978
Ivan Fomin <la-verne@mail.ru>
2018-01-10 10:24:49 +0300

    Changed index

--- HomeOtus1/index.php
+++ HomeOtus1/index.php
@@ -3,14 +3,14 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 echo "Enter the path for the file: ";
 
-$line = trim(fgets(STDIN));
+$file = trim(fgets(STDIN));
 
-$string = file_get_contents($line);
+$string = file_get_contents($file);
 
 $helper = new Helper();
 
 try {
     echo $helper->matchBrackets($string) ? 'true' . PHP_EOL : 'false' . PHP_EOL;
 } catch (InvalidArgumentException $exception) {
-    echo "Не верные данные" . PHP_EOL;
+    echo "Не верные данные в файле " . $file . PHP_EOL;
 }
